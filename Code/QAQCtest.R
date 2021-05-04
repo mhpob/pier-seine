f2a60f5f3854b2d3d136bf6e4a747853cf675fc5
flyfrey <44652714+flyfrey@users.noreply.github.com>
2021-05-04 14:29:21 -0400

    Update QAQCtest.R

--- Code/QAQCtest.R
+++ Code/QAQCtest.R
@@ -5,17 +5,4 @@ lengths[, record_num := as.factor(record_num)]
 
 head(lengths)
 
-inland <- lengths[grepl('beryllina', scientific)]
-inland[, wk := week(date)]
-inland[, yr_fac := as.factor(year)]
-
-
-
-mod <- gam(list(length ~ s(wk, by = yr_fac) + yr_fac,
-                ~ s(wk, k = 15)),
-           data = inland, 
-           family = 'gaulss')
-plot(mod, scale = 0, shade = T, pages = 1)
-
-summary(lengths)
-
+unique(lengths$scientific)
