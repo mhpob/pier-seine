83cec16c723c6ff8e59ac23afdf442dbfce8cc7b
flyfrey <44652714+flyfrey@users.noreply.github.com>
2021-05-13 15:52:19 -0400

    Made a summary for meeting.

--- Code/QAQCtest.R
+++ Code/QAQCtest.R
@@ -47,13 +47,23 @@ b1 <- ggplot(data = a.ss, ) +
 
 ggplotly(b1)
 
+#Outlier Function
+outlierReplace = function(dataframe, cols, rows, newValue = NA) {
+  if (any(rows)) {
+    set(dataframe, rows, cols, newValue)
+  }
+}
+
+
+
+
 # A.ss Outliers:(Record #, Size)
   #(1721, 703) (1733, 706)
-  #MAybe (3067, 199) Check LH Book, "No bigger than 153mm"
-  
-qqnorm(a.ss$length)
-qqline(a.ss$length)
+  #MAybe (3067, 199) Check LH Book, "No bigger than max lengths of 180mm" 
+  #YOY max length 80mm suggested by books
 
+
+  #QQ Plot Function
   gg_qq <- function(x, distribution = "norm", ..., line.estimate = NULL, conf = 0.95,
                     labels = names(x)){
     q.function <- eval(parse(text = paste0("q", distribution)))
@@ -93,8 +103,11 @@ qqline(a.ss$length)
   }  
   
 gg_qq(a.ss$length)
-m1 <- lm(a.ss$wk ~ a.ss$length)
+
 IQR(a.ss$length)
+
+outlierReplace(a.ss, "length", which(a.ss$length > 180), NA)  
+ggplotly(b1)
   
 ##Atlantic needlefish
 need <- lengths[grepl('Strongylura', scientific)]
@@ -108,8 +121,15 @@ p4 <- ggplot(data = need) +
   geom_point(aes(x = length, y = 1, color = record_num), show.legend = FALSE)
 
 ggplotly(p4)
-#Max length Needlefish : 1110 mm TL, expected at sampling period ?
+
+b2 <- ggplot(data = need, ) +
+  geom_boxplot(aes(x = "", y = length, ), show.legend=FALSE) + coord_flip()
+
+ggplotly(b2)
+
 gg_qq(need$length)
+#Max length Needlefish : 1110 mm TL, expected at sampling period ?
+
 
 ##Blue Crab
 crab <- lengths[grepl('sapidus', scientific)]
@@ -124,6 +144,11 @@ p6 <- ggplot(data = crab) +
 
 ggplotly(p6)
 
+b3 <- ggplot(data = crab, ) +
+  geom_boxplot(aes(x = "", y = length, ), show.legend=FALSE) + coord_flip()
+
+ggplotly(b3)
+
 gg_qq(crab$length)
 #Spot
 spot <- lengths[grepl('xanthurus', scientific)]
@@ -137,14 +162,25 @@ p8 <- ggplot(data = spot) +
   geom_point(aes(x = length, y = 1, color = record_num), show.legend = FALSE)
 
 ggplotly(p8)
-  #Outliers:(Record #, Size)
+
+b4 <- ggplot(data = spot, ) +
+  geom_boxplot(aes(x = "", y = length, ), show.legend=FALSE) + coord_flip()
+
+ggplotly(b4)  
+
+gg_qq(spot$length)
+
+#Outliers:(Record #, Size)
         #(1651, 409) Max 360mm
+outlierReplace(spot, "length", which(spot$length > 360), NA)  
+ggplotly(p7)
+ggplotly(p8)
+ggplotly(b4)
 gg_qq(spot$length)
 
 ###inland silverside###
 inland <- lengths[grepl('beryllina', scientific)]
-inland[, wk := week(date)]
-inland[, yr_fac := as.factor(year)]
+
 p9 <- ggplot(data = inland) + 
   geom_histogram(aes(x = length))
 
@@ -154,9 +190,21 @@ p10 <- ggplot(data = inland) +
   geom_point(aes(x = length, y = 1, color = record_num), show.legend = FALSE)
 
 ggplotly(p10)
+
+b5 <- ggplot(data = inland, ) +
+  geom_boxplot(aes(x = "", y = length, ), show.legend=FALSE) + coord_flip()
+
+ggplotly(b5)
+
+gg_qq(inland$length)
+
 #Outliers:(Record #, Size)
       #(1598, 700) Max 150mm
 
+outlierReplace(inland, "length", which(inland$length > 150), NA)  
+ggplotly(p9)
+ggplotly(p10)
+ggplotly(b5)
 gg_qq(inland$length)
 
 ##striped bass##
@@ -173,6 +221,11 @@ p12 <- ggplot(data = bass) +
 
 ggplotly(p12)
 
+b6 <- ggplot(data = bass, ) +
+  geom_boxplot(aes(x = "", y = length, ), show.legend=FALSE) + coord_flip()
+
+ggplotly(b6)
+
 gg_qq(bass$length)
 
 #bluefish#
@@ -188,9 +241,19 @@ p14 <- ggplot(data = blue) +
   geom_point(aes(x = length, y = 1, color = record_num), show.legend = FALSE)
 
 ggplotly(p14)
-   #Outliers:(Record #, Size)
-    #(1668, 700) Max 1130mm
+   
+b7 <- ggplot(data = blue, ) +
+  geom_boxplot(aes(x = "", y = length, ), show.legend=FALSE) + coord_flip()
 
+ggplotly(b7)
+
+gg_qq(blue$length)
+#Outliers:(Record #, Size)
+    #(1668, 700) Max 1130mm
+outlierReplace(blue, "length", which(blue$length > 1130), NA)  
+ggplotly(p13)
+ggplotly(p14)
+ggplotly(b7)
 gg_qq(blue$length)
 
 ##bay anchovy##
@@ -206,9 +269,20 @@ p16 <- ggplot(data = ancho) +
   geom_point(aes(x = length, y = 1, color = record_num), show.legend = FALSE)
 
 ggplotly(p16)
+
+b8 <- ggplot(data = ancho, ) +
+  geom_boxplot(aes(x = "", y = length, ), show.legend=FALSE) + coord_flip()
+
+ggplotly(b8)
+
+gg_qq(ancho$length)
     #Outliers:(Record #, Size)
       #(3100, 186) Max 110mm
-
+outlierReplace(ancho, "length", which(ancho$length > 110), NA)  
+ggplotly(p15)
+ggplotly(p16)
+ggplotly(b8)
+gg_qq(ancho$length)
 ##menhaden##
 
 men <- lengths[grepl('Brevoortia tyrannus', scientific)]
@@ -222,6 +296,13 @@ p18 <- ggplot(data = men) +
   geom_point(aes(x = length, y = 1, color = record_num), show.legend = FALSE)
 
 ggplotly(p18)
+men
+b9 <- ggplot(data = men, ) +
+  geom_boxplot(aes(x = "", y = length, ), show.legend=FALSE) + coord_flip()
+
+ggplotly(b9)
+
+gg_qq(men$length)
  #max 500mm
 
 ##gizzard shad##
@@ -237,19 +318,34 @@ p20 <- ggplot(data = shad) +
   geom_point(aes(x = length, y = 1, color = record_num), show.legend = FALSE)
 
 ggplotly(p20)
+
+b10 <- ggplot(data = shad, ) +
+  geom_boxplot(aes(x = "", y = length, ), show.legend=FALSE) + coord_flip()
+
+ggplotly(b10)
+
+gg_qq(shad$length)
+
 #max 570mm
 
 ##white perch##
 
 perch <- lengths[grepl('Morone americana', scientific)]
 
-p21 <- ggplot(data = men) + 
+p21 <- ggplot(data = perch) + 
   geom_histogram(aes(x = length))
 
 ggplotly(p21)
 
-p22 <- ggplot(data = men) + 
+p22 <- ggplot(data = perch) + 
   geom_point(aes(x = length, y = 1, color = record_num), show.legend = FALSE)
 
 ggplotly(p22)
+
+b11 <- ggplot(data = perch, ) +
+  geom_boxplot(aes(x = "", y = length, ), show.legend=FALSE) + coord_flip()
+
+ggplotly(b11)
+
+gg_qq(perch$length)
 #max 580mm
